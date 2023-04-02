e53d9086889e6a67e27440dd75069f7453120f17
Wynter Jones <34955844+WynterJones@users.noreply.github.com>
2023-04-02 18:52:33 -0400

    updates

---
+++ app/commands/ping.js
@@ -0,0 +1,10 @@
+/*
+ *   Copyright (c) 2023 Wynter Jones
+ *   All rights reserved.
+ */
+
+function ping(message) {
+  message.channel.send('Pong!')
+}
+
+module.exports = ping
---
+++ app/index.js
@@ -0,0 +1,35 @@
+/*
+ *   Copyright (c) 2023 Wynter Jones
+ *   All rights reserved.
+ */
+
+require('dotenv').config()
+const { Client, Events, GatewayIntentBits } = require('discord.js')
+const requireDir = require('require-dir')
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+})
+const commands = requireDir('./commands')
+const command_prefix = '!!'
+
+client.once(Events.ClientReady, () => console.log('Bot is Ready'))
+
+client.on('messageCreate', message => {
+  if (!message.author.bot && message.content.startsWith(command_prefix)) {
+    runCommand(message)
+  }
+})
+
+const runCommand = message => {
+  const command = message.content.slice(command_prefix.length).trim()
+  commands[command]
+    ? commands[command](message)
+    : message.channel.send('Invalid command!')
+}
+
+client.login(process.env.DISCORD_AUTH_TOKEN)
